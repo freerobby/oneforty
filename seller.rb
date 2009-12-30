f43d98032f3cda3ab1b6c29285fb03308db5f81c
Michael Macasek <michael@oneforty.com>
2009-12-30 23:37:10 +0800

    Move to JSON per API spec

--- seller.rb
+++ seller.rb
@@ -4,17 +4,18 @@ require 'rest_client'
 require 'json'
 
 post '/sell' do
+  post_data = JSON.load(request.body.read.to_s)
   oneforty = RestClient::Resource.new("https://" + ENV['ONEFORTY_ENV'],
                                       :verify_ssl => OpenSSL::SSL::VERIFY_NONE)
 
-  json = { :reference_code => params[:reference_code],
-           :developer_key  => ENV['DEVELOPER_KEY'] }
+  json = { :reference_code => post_data["reference_code"],
+           :developer_key  => ENV['DEVELOPER_KEY'] }.to_json
 
   ack_response = JSON.parse(oneforty['/fulfillment/acknowledge'].post(json))
 
-  json = { :reference_code       => params[:reference_code],
+  json = { :reference_code       => post_data["reference_code"],
            :developer_key        => ENV['DEVELOPER_KEY'],
-           :buyer_twitter_handle => ack_response['buyer_twitter_handle'] }
+           :buyer_twitter_handle => ack_response['buyer_twitter_handle'] }.to_json
 
   complete_response = oneforty['/fulfillment/complete'].post(json)
   
